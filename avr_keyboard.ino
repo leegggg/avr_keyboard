09dc7bc276ecdaa31449e4bb9003e59d23048dd0
LIN Yizhou <yizhou.lin@outlook.com>
2024-08-20 10:12:20 +0800

    init

---
+++ avr_keyboard.ino
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <SerialCommand.h>
+
+SerialCommand cli;
+
+
+void sendOK(){
+    Serial.print("OK\r\n"); /* Send "OK" message */
+}
+
+void sendError(){
+    Serial.print("ERROR\r\n"); /* Send "OK" message */
+}
+
+void keyBoardText(){
+    char* arg = cli.next();
+    if(!arg){
+        sendError();
+        return;
+    }
+    Serial.print("+TEXT:");
+    Serial.print(arg);
+    Serial.print("\r\n");
+    sendOK();
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  cli.addCommand((char *)"AT+KBTEXT", sendOK, sendError, keyBoardText, sendError);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  cli.loop();
+}
